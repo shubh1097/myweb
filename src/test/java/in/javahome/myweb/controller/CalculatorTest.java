51a248ae5db416ee59ba0776567bc42898b0cdf6
shubh1097 <80589319+shubh1097@users.noreply.github.com>
2021-07-22 12:45:28 +0530

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -4,11 +4,5 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class CalculatorTest extends TestCase {
-	Calculator cal = new Calculator();
-	public void testAdd(){
-		Assert.assertEquals(cal.add(10, 20), 30);
-	}
-	public void testMultiply(){
-		Assert.assertEquals(cal.multiply(10, 20), 200);
-	}
+	
 }
